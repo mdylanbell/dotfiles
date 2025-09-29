6cf86a342779ca56d1ff97d4788d3cff05631a9f
Matthew Bell <m.dylan.bell@gmail.com>
2025-09-28 23:39:24 -0700

    Refactor and reduce mason, conform, and none-ls plugins

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,28 +1,12 @@
--- Formatting via Conform, using CLIs installed with mise (on PATH).
+-- ~/.config/nvim/lua/plugins/conform.lua
 return {
-  "stevearc/conform.nvim",
-  opts = function(_, opts)
-    opts.formatters_by_ft = opts.formatters_by_ft or {}
-    local fb = opts.formatters_by_ft
-
-    -- Python: ruff fix (incl. import sorting) then ruff format
-    fb.python = { "ruff_fix", "ruff_format" }
-
-    -- Lua / TOML
-    fb.lua = { "stylua" }
-    fb.toml = { "taplo" }
-
-    -- JS/TS/JSON/YAML/Markdown
-    fb.javascript = { "prettier" }
-    fb.typescript = { "prettier" }
-    fb.javascriptreact = { "prettier" }
-    fb.typescriptreact = { "prettier" }
-    fb.json = { "prettier" }
-    fb.yaml = { "prettier" }
-    fb.markdown = { "prettier" }
-
-    -- Terraform (only works on hosts where terraform is present; enabled via MISE_ENV=work)
-    fb.terraform = { "terraform_fmt" }
-    fb["terraform-vars"] = { "terraform_fmt" }
-  end,
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      -- Ruff CLI: fix, then organize imports, then format
+      -- Names per Conform & Ruff docs.
+      opts.formatters_by_ft.python = { "ruff_fix", "ruff_organize_imports", "ruff_format" }
+    end,
+  },
 }
