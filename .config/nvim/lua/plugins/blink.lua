9b2467f815992e8c621c55df53efd53e7cd7f2bc
Matthew Bell <m.dylan.bell@gmail.com>
2025-10-09 11:55:32 -0700

    nvim: Configure blink to not preselect first suggestion

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      completion = {
+        list = {
+          selection = {
+            -- don't preselect the first item
+            preselect = false,
+          },
+        },
+      },
+    },
+  },
+}
