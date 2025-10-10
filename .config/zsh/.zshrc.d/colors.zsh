35c798ac3c8301a99b63ebb778a0b80dd90cc5bb
Matthew Bell <m.dylan.bell@gmail.com>
2025-10-10 15:36:12 -0700

    zsh: colorize -h/--help with bat if available

--- .config/zsh/.zshrc.d/colors.zsh
+++ .config/zsh/.zshrc.d/colors.zsh
@@ -4,3 +4,8 @@ else
   DIRCOLORS=dircolors
 fi
 eval `$DIRCOLORS "$XDG_CONFIG_HOME"/dircolors`
+
+if (( $+commands[bat] )); then
+  alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+  alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+fi
