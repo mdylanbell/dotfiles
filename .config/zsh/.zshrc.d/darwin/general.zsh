50752cd26429b2304eceaa10fef4257ae7156550
Matthew Bell <m.dylan.bell@gmail.com>
2025-10-10 03:25:39 -0700

    zsh: Update os specific configs

--- .config/zsh/.zshrc.d/darwin/general.zsh
+++
@@ -1,5 +0,0 @@
-# python/virtualenv
-# PYTHON_BIN_DIR=/usr/local/bin
-# export WORKON_HOME=$HOME/.virtual_envs
-# export VIRTUALENVWRAPPER_PYTHON=$PYTHON_BIN_DIR/python3
-# [[ -s "/usr/local/bin/virtualenvwrapper.sh" ]] && source /usr/local/bin/virtualenvwrapper.sh
---
+++ .config/zsh/.zshrc.d/linux-gnu/brew.zsh
@@ -0,0 +1,27 @@
+# find or set HOMEBREW_PREFIX
+if [[ -z "$HOMEBREW_PREFIX" ]]; then
+  if [[ -d /home/linuxbrew/.linuxbrew ]]; then
+    HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+  fi
+fi
+
+# activate brew
+if [[ -x "$HOMEBREW_PREFIX"/bin/brew ]]; then
+  eval "$(${HOMEBREW_PREFIX}/bin/brew shellenv)"
+fi
+
+# configure getopt if present
+if [[ -x "$HOMEBREW_PREFIX"/opt/gnu-getopt/bin/getopt ]]; then
+  export FLAGS_GETOPT_CMD="$HOMEBREW_PREFIX/opt/gnu-getopt/bin/getopt"
+fi
+
+# configure CC if present
+if [[ -x "$HOMEBREW_PREFIX"/bin/gcc-15 ]]; then
+  export CC=/home/linuxbrew/.linuxbrew/bin/gcc-15
+fi
+# configure openssl if present
+
+if [[ -x "$HOMEBREW_PREFIX"/opt/openssl@3/bin/openssl ]]; then
+  export LDFLAGS="-L$HOMEBREW_PREFIX/opt/openssl@3/lib"
+  export CPPFLAGS="-I$HOMEBREW_PREFIX/opt/openssl@3/include"
+fi
