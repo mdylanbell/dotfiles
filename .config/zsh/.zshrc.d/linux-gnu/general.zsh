a71c3703a8faff5c4ee50248bcb82408f3dbd388
Matthew Bell <m.dylan.bell@gmail.com>
2025-10-10 03:25:39 -0700

    zsh: Update os specific configs

--- .config/zsh/.zshrc.d/linux-gnu/general.zsh
+++
@@ -1,6 +0,0 @@
-# python/virtualenv
-PYTHON_BIN_DIR=/usr/bin
-export WORKON_HOME=$HOME/.virtual_envs
-export VIRTUALENVWRAPPER_PYTHON=$PYTHON_BIN_DIR/python3
-[[ -s "$HOME/.local/bin/virtualenvwrapper.sh" ]] && source $HOME/.local/bin/virtualenvwrapper.sh
-
