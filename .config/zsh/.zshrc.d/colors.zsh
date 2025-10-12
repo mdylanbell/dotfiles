125156b7439c1e67f2c80c8e8d5c5eaf106625c7
Matthew Bell <m.dylan.bell@gmail.com>
2025-10-12 16:19:22 -0700

    zsh: add comment on bat help colorizing

--- .config/zsh/.zshrc.d/colors.zsh
+++ .config/zsh/.zshrc.d/colors.zsh
@@ -5,6 +5,7 @@ else
 fi
 eval `$DIRCOLORS "$XDG_CONFIG_HOME"/dircolors`
 
+# colorize help messages (-h and --help args) for commands
 if (( $+commands[bat] )); then
   alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
   alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
