e6230fb9c9d32f55ad2f5b6189e2146480303181
Matthew Bell <m.dylan.bell@gmail.com>
2025-10-13 19:02:59 -0700

    nvim: simplify conform config for python

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -1,11 +1,10 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = function(_, opts)
-      opts.formatters_by_ft = opts.formatters_by_ft or {}
-      -- Ruff CLI: fix, then organize imports, then format
-      -- Names per Conform & Ruff docs.
-      opts.formatters_by_ft.python = { "ruff_fix", "ruff_organize_imports", "ruff_format" }
-    end,
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
+      },
+    },
   },
 }
