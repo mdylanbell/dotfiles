e1147c3ef90adaa76f1905de9d75e7914973a876
Matthew Bell <m.dylan.bell@gmail.com>
2025-12-06 02:21:30 -0800

    nvim: cleanup sh.lua to rely on table merges

--- .config/nvim/lua/plugins/sh.lua
+++ .config/nvim/lua/plugins/sh.lua
@@ -1,16 +1,14 @@
 return {
   "stevearc/conform.nvim",
-  opts = function(_, opts)
-    opts.formatters_by_ft = opts.formatters_by_ft or {}
-    -- shfmt supports POSIX sh and bash (not zsh), so wire it to these FTs:
-    opts.formatters_by_ft.sh = { "shfmt" }
-    opts.formatters_by_ft.bash = { "shfmt" }
-
-    opts.formatters = opts.formatters or {}
-    -- Add your desired args
-    opts.formatters.shfmt = {
-      -- use prepend_args so we keep any sane defaults Conform sets
-      prepend_args = { "-i", "2", "-ci", "-bn" },
-    }
-  end,
+  opts = {
+    formatters_by_ft = {
+      sh = { "shfmt" },
+      bash = { "shfmt" },
+    },
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2", "-ci", "-bn" },
+      },
+    },
+  },
 }
