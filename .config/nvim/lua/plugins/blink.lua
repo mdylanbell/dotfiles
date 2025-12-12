0b4fa3367e2d9973f3c4d15bfd896e28ce3ad2d8
Matthew Bell <m.dylan.bell@gmail.com>
2025-12-12 10:52:59 -0800

    nvim: blink: show hidden fiels by default on path completion

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -11,6 +11,16 @@ return {
           },
         },
       },
+      sources = {
+        providers = {
+          path = {
+            opts = {
+              -- show hidden files in path completions
+              show_hidden_files_by_default = true,
+            },
+          },
+        },
+      },
     },
   },
 }
