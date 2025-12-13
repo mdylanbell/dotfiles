6a0f98dd9da7198b5050ef7c3f3051c375a3c4cf
Matthew Bell <m.dylan.bell@gmail.com>
2025-12-13 14:42:12 -0800

    zsh/fzf: Fix colorscheme config so it actually works

--- .config/zsh/conf.d/70-tools/fzf.zsh
+++ .config/zsh/conf.d/70-tools/fzf.zsh
@@ -1,7 +1,12 @@
 # use fd, follow links, show hidden files, follow symlinks
 export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
 
-# Pluggable themes
-_FZF_COLORS_SOLARIZED_DARK="bg+:-1,bg:-1,fg:254,fg+:254,hl:61,hl+:61,info:136,prompt:136,pointer:61,marker:61,spinner:136,header:136,border:254"
-_FZF_COLORS_CATPPUCCIN_MOCHA="bg:#1e1e2e,bg+:#313244,fg:#cdd6f4,fg+:#f5e0dc,hl:#f38ba8,hl+:#fab387,info:#cba6f7,prompt:#89b4fa,pointer:#f5c2e7,marker:#a6e3a1,spinner:#f5e0dc,header:#f38ba8,border:#585b70"
-export FZF_COLORS="$_FZF_COLORS_CATPPUCCIN_MOCHA"
+# https://github.com/catppuccin/fzf/blob/main/themes/catppuccin-fzf-mocha.sh
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+--color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+--color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
+--color=selected-bg:#45475A \
+--color=border:#6C7086,label:#CDD6F4"
+
+# TODO: Also define solarized colors and make FZF_DEFAULT_OPTS = selected scheme
