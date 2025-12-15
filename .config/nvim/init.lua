2e5caddeaa9f2ea5603d974d9773d1eb15c85fe6
Matthew Bell <m.dylan.bell@gmail.com>
2025-12-14 16:22:39 -0800

    nvim: expand python host path

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- set python
-vim.g.python3_host_prog = "$XDG_CACHE_HOME/venv/default/bin/python"
+-- Expand XDG_CACHE_HOME so provider resolves to the actual venv path
+vim.g.python3_host_prog = vim.fn.expand("$XDG_CACHE_HOME/venv/default/bin/python")
 
 -- disable latex rendering
 require("render-markdown").setup({ latex = { enabled = false } })
