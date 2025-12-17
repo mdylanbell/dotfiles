947fcdc1f330486c25ebe9b54caf3c2a74f9c6d4
Matthew Bell <m.dylan.bell@gmail.com>
2025-12-16 16:51:21 -0800

    zsh+mise: Set default MISE_ENV to personal, assume override in local.zsh if needed

--- .config/zsh/conf.d/00-env.zsh
+++ .config/zsh/conf.d/00-env.zsh
@@ -12,3 +12,6 @@ export ZSH_OS
 
 # history location (XDG state)
 export HISTFILE="$XDG_STATE_HOME"/zsh/history
+
+# set default mise env; override in local.zsh if something else is needed (e.g. work)
+export MISE_ENV=personal
