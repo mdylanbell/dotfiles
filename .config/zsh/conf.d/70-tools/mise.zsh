61f5f9a47b436796955c4052101c762b0628019f
Matthew Bell <m.dylan.bell@gmail.com>
2026-01-06 11:09:39 -0800

    mise: Add default node modules

--- .config/zsh/conf.d/70-tools/mise.zsh
+++ .config/zsh/conf.d/70-tools/mise.zsh
@@ -1,7 +1,11 @@
+# NOTE: Currently using a zinit plugin for mise activation and copmletions
 # Activate mise (version/runtime manager) if present
 # if (( $+commands[mise] )); then
 #   eval "$(mise activate zsh)"
 # fi
 
-# Default Python packages installed by mise after python install/upgrade
-export MISE_PYTHON_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME/mise/default-python-packages"
+# Default python packages installed by mise after python install/upgrade
+export MISE_PYTHON_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME"/mise/default-python-packages
+
+# Default node packages installed by mise after python install/upgrade
+export MISE_NODE_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME"/mise/default-node-packages
