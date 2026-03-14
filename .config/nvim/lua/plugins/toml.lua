7ef68145e427f080c1cdd15c86f2ca090a359f4f
Matthew Bell <m.dylan.bell@gmail.com>
2026-03-13 17:17:37 -0700

    nvim: fix taplo/toml config

--- .config/nvim/lua/plugins/toml.lua
+++ .config/nvim/lua/plugins/toml.lua
@@ -1,10 +1,16 @@
+-- Preserve Taplo's normal project root markers, but fall back to the file's
+-- directory so the LSP still attaches for detached TOML files.
+local markers = { ".taplo.toml", "taplo.toml", ".git" }
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
       taplo = {
-        -- enable taplo to work on any detached .toml file or any toml under ~/.config
-        root_markers = { ".config", ".taplo.toml", "taplo.toml", ".git", "*.toml" },
+        root_markers = markers,
+        root_dir = function(fname)
+          return vim.fs.root(fname, markers) or vim.fs.dirname(fname)
+        end,
       },
     },
   },
