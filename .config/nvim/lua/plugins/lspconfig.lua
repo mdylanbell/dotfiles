840fcf22c1c44b8f13f67b0a0bba03dfac1a6b60
Matthew Bell <m.dylan.bell@gmail.com>
2026-03-13 17:17:07 -0700

    nvim(lspconfig): add workaround for lazydev issue 136

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -11,6 +11,25 @@ return {
           },
         },
       },
+      -- Work around lazydev/lua_ls config timing issues for Neovim globals:
+      -- https://github.com/folke/lazydev.nvim/issues/136
+      lua_ls = {
+        settings = {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+              path = { "?.lua", "?/init.lua" },
+            },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+                vim.fn.stdpath("config") .. "/lua",
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
