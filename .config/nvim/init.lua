c065d4ebc0206095520718f58d09ec696265df0c
Matthew Bell <m.dylan.bell@gmail.com>
2026-03-19 03:22:19 -0700

    nvim: reorganize provider handling, disable ruby

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- Expand XDG_CACHE_HOME so provider resolves to the actual venv path
-vim.g.python3_host_prog = vim.fn.expand("$XDG_CACHE_HOME/venv/default/bin/python")
-
 -- disable latex rendering
 require("render-markdown").setup({ latex = { enabled = false } })
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,14 +2,26 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+---- Configure providers
+
+-- Expand XDG_CACHE_HOME so provider resolves to the actual venv path
+vim.g.python3_host_prog = vim.fn.expand("$XDG_CACHE_HOME/venvs/nvim/bin/python")
+
+-- Disable Perl provider
+vim.g.loaded_perl_provider = 0
+
+-- Disable Ruby provider
+vim.g.loaded_ruby_provider = 0
+
+---- Configure python options
+
 -- Prefer pyright (standard) for Python LSP
 vim.g.lazyvim_python_lsp = "basedpyright"
 
 -- Prefer ruff over black
 vim.g.lazyvim_python_ruff = "ruff"
 
--- Disable Perl provider
-vim.g.loaded_perl_provider = 0
+---- Add custom ReviewPR command
 
 vim.api.nvim_create_user_command("ReviewPR", function()
   require("config.review").review_pr_command()
