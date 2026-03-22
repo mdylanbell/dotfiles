1d36ca337d33a42bd25634c89fb649c81209d38f
Matthew Bell <m.dylan.bell@gmail.com>
2026-03-21 19:21:55 -0700

    nvim: remove lua_ls workaround after upstream fix

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -11,25 +11,6 @@ return {
           },
         },
       },
-      -- Work around lazydev/lua_ls config timing issues for Neovim globals:
-      -- https://github.com/folke/lazydev.nvim/issues/136
-      lua_ls = {
-        settings = {
-          Lua = {
-            runtime = {
-              version = "LuaJIT",
-              path = { "?.lua", "?/init.lua" },
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME,
-                vim.fn.stdpath("config") .. "/lua",
-              },
-            },
-          },
-        },
-      },
     },
   },
 }
