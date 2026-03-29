3252c5b3c04abfb60f6587c3f11388e765b4e120
Matthew Bell <m.dylan.bell@gmail.com>
2026-03-28 17:04:48 -0700

    zsh: add wsl as OS option, add MISE_ENV for os

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -1,9 +1,13 @@
 # Detect OS
-case $OSTYPE in
-  darwin*) ZSH_OS=darwin ;;
-  linux*) ZSH_OS=linux ;;
-  *) ZSH_OS=${OSTYPE%%-*} ;;
-esac
+if [[ "$(uname -r)" == *[Mm]icrosoft* ]]; then
+  ZSH_OS="wsl"
+else
+  case $OSTYPE in
+    darwin*) ZSH_OS=darwin ;;
+    linux*) ZSH_OS=linux ;;
+    *) ZSH_OS=${OSTYPE%%-*} ;;
+  esac
+fi
 export ZSH_OS
 
 if [[ -r "${ZDOTDIR:-$HOME/.config/zsh}/env.local.zsh" ]]; then
@@ -14,7 +18,7 @@ export DOTFILES_ENV="${DOTFILES_ENV:-personal}"
 
 # ---- mise env ----
 # Let MISE_ENV follow DOTFILES_ENV by default, but preserve an explicit override.
-export MISE_ENV="${MISE_ENV:-$DOTFILES_ENV}"
+export MISE_ENV="${MISE_ENV:-$DOTFILES_ENV},${ZSH_OS}"
 
 # ---- secrets / integrations ----
 export DOTFILES_1PASSWORD_VAULT="${DOTFILES_1PASSWORD_VAULT:-dotfiles-${DOTFILES_ENV}}"
