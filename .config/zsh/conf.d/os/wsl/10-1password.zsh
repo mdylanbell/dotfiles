b7937f781adab49180f3593a26253e84205c4bde
Matthew Bell <m.dylan.bell@gmail.com>
2026-03-28 17:09:38 -0700

    zsh/mise/doc: Add config for wsl for 1password

---
+++ .config/zsh/conf.d/os/wsl/10-1password.zsh
@@ -0,0 +1,6 @@
+# export SSH_AUTH_SOCK="${HOME}/.1password/agent.sock"
+
+# https://developer.1password.com/docs/ssh/integrations/wsl/
+alias ssh="ssh.exe"
+alias ssh-add="ssh-add.exe"
+export GIT_SSH_COMMAND="ssh.exe"
