203bd15e71fde32f21711f6379a289c9905bc075
Matthew Bell <m.dylan.bell@gmail.com>
2026-06-19 10:52:25 -0700

    nvim: add tfrc filetype as terraform

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -1,4 +1,7 @@
 vim.filetype.add({
+  extension = {
+    tfrc = "terraform",
+  },
   pattern = {
     ["${XDG_CONFIG_HOME}/git/config%..*"] = "gitconfig",
     ["${DOTFILES_ROOT}/%.config/git/config%..*"] = "gitconfig",
