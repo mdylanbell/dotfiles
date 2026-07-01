3abfa3e34a6469c3f903ce4687bf21259053c04b
Matthew Bell <m.dylan.bell@gmail.com>
2026-06-30 18:01:38 -0700

    nvim: taplo mostly died, use tombi for toml

--- .config/nvim/lua/plugins/toml.lua
+++ .config/nvim/lua/plugins/toml.lua
@@ -1,19 +1,23 @@
 -- Treat only explicit Taplo config files as project roots. Otherwise, use the
 -- TOML file's own directory so Taplo still attaches for detached dotfile
 -- configs inside larger repos.
-local markers = { ".taplo.toml", "taplo.toml" }
+-- local markers = { ".taplo.toml", "taplo.toml" }
 
 return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      tombi = {},
       taplo = {
-        root_markers = markers,
-        root_dir = function(bufnr, on_dir)
-          local fname = vim.api.nvim_buf_get_name(bufnr)
-          on_dir(vim.fs.root(fname, markers) or vim.fs.dirname(fname))
-        end,
+        enabled = false,
       },
+      -- taplo = {
+      --   root_markers = markers,
+      --   root_dir = function(bufnr, on_dir)
+      --     local fname = vim.api.nvim_buf_get_name(bufnr)
+      --     on_dir(vim.fs.root(fname, markers) or vim.fs.dirname(fname))
+      --   end,
+      -- },
     },
   },
 }
