13a28716f73caecea6fcef3ab6103d3b149a25ad
Matthew Bell <m.dylan.bell@gmail.com>
2026-08-04 18:58:00 -0700

    nvim: update plugins, add plenary to neotest

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,11 +1,33 @@
 return {
   "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-neotest/neotest-plenary",
+  },
   opts = {
     adapters = {
       ["neotest-python"] = {
         -- Add --no-cov to pytest arguments
         args = { "--no-cov" },
       },
+      -- Runs plenary.nvim busted-style `*_spec.lua` tests (e.g. taskfile.nvim).
+      ["neotest-plenary"] = {},
+    },
+    discovery = {
+      -- Don't descend into vendored dependency / build output dirs. Otherwise
+      -- neotest discovers the `*_spec.lua` files shipped inside cloned deps
+      -- (e.g. `build/ext/plenary.nvim`) and tries to run them.
+      filter_dir = function(name)
+        local ignored = {
+          build = true,
+          [".git"] = true,
+          node_modules = true,
+          [".venv"] = true,
+          venv = true,
+          target = true,
+        }
+        return not ignored[name]
+      end,
     },
   },
 }
